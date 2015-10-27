0073816d58aeb3e51d28aefe1cb6173b89cb7d4d
Rafael Arkenau <Th3f@users.noreply.github.com>
2015-10-27 20:36:36 +0100

    Translated 404-Page and removed Uberspace-Badge

--- 404.php
+++ 404.php
@@ -3,9 +3,9 @@
 			<div id="main">
 				<section id="one">
 					<header class="major">
-						<h1>404 - Nicht gefunden!</h1>
+						<h1>404 - Not found!</h1>
 					</header>
-					<p>Die mittlerweile todmüden aber immernoch hochtrainierten YouTube-Affen konnten den von Dir angeforderten Inhalt leider nirgends auftreiben... Du kannst allerdings mal danach suchen, wenn Du magst:</p>
+					<p>Unfortunately, the highly trained YouTube-monkeys were not able to find the file, you were looking for... Maybe, you can try a search?</p>
 					<form class="site-search" action="<?php echo search_url(); ?>" method="post">
 						<div class="row uniform 50%">
 							<div class="6u 12u$(xsmall)">
@@ -14,11 +14,6 @@
 						</div>
 					</form>
 				</section>
-				<div class="uberspace">
-					<a href="https://uberspace.de" target="_blank">
-						<img src="https://th3f.de/themes/Strata/images/uberspace.png" alt="Hosted on Asteroids!">
-					</a>
-				</div>
 			</div>
 
 <?php theme_include('footer'); ?>
