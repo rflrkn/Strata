0de5b445d9df878941bbe48fc537d3d1c7e52041
Rafael Arkenau <Th3f@users.noreply.github.com>
2015-10-27 20:39:50 +0100

    Translated articles-Page and removed Uberspace-Badge

--- article.php
+++ article.php
@@ -4,29 +4,29 @@
 				<section id="one">
 					<header class="major">
 						<h1><?php echo article_title(); ?></h1>
-						<p>Veröffentlicht am <?php echo article_date(); ?> von <?php echo article_author('real_name'); ?></p>
+						<p>Published on <?php echo article_date(); ?> by <?php echo article_author('real_name'); ?></p>
 					</header>
 					<?php echo article_markdown(); ?>
 				</section>
 				<!-- Comment Form -->
 				<?php if(comments_open()): ?>
 						<section>
-							<h4>Kommentare</h4>
+							<h4>Comments</h4>
 							<form id="comment" method="post" action="<?php echo comment_form_url(); ?>#comment">
 								<?php echo comment_form_notifications(); ?>
 								<div class="row uniform 50%">
 									<div class="6u 12u$(xsmall)">
-										<?php echo comment_form_input_name('placeholder="Dein Name"'); ?>
+										<?php echo comment_form_input_name('placeholder="Name"'); ?>
 									</div>
 									<div class="6u$ 12u$(xsmall)">
-										<?php echo comment_form_input_email('placeholder="Deine Emailaddresse (wird nicht veröffentlicht)"'); ?>
+										<?php echo comment_form_input_email('placeholder="E-Mail adress"'); ?>
 									</div>
 									<div class="12u$">
-										<?php echo comment_form_input_text('placeholder="Dein Kommentar (Zeilenumbrüche werden entfernt)"'); ?>
+										<?php echo comment_form_input_text('placeholder="Comment"'); ?>
 									</div>
 									<div class="12u$">
 										<ul class="actions">
-											<li><input type="submit" value="Gib deinen Senf ab!" class="special" /></li>
+											<li><input type="submit" value="Lets go!" class="special" /></li>
 										</ul>
 									</div>
 								</div>
@@ -50,11 +50,6 @@
 							<?php endif; ?>
 						</section>
 				<?php endif; ?>
-				<div class="uberspace">
-					<a href="https://uberspace.de" target="_blank">
-						<img src="https://th3f.de/themes/Strata/images/uberspace.png" alt="Hosted on Asteroids!">
-					</a>
-					</div>
 			</div>
 
 <?php theme_include('footer'); ?>
