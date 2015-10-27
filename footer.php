6f625b4ad5259516092586c88b9a73aeafee30a3
Rafael Arkenau <Th3f@users.noreply.github.com>
2015-10-27 20:41:43 +0100

    Removed some shit I forgot to remove... Shame on me...

--- footer.php
+++ footer.php
@@ -1,16 +1,14 @@
 		<!-- Footer -->
 			<footer id="footer">
 				<ul class="icons">
-					<li><a href="https://twitter.com/misformado" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-					<li><a href="https://plus.google.com/+RafaelArkenau" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
-					<li><a href="https://www.flickr.com/photos/th3f" class="icon fa-flickr"><span class="label">Flickr</span></a></li>
-					<li><a href="https://steamcommunity.com/id/th3f" class="icon fa-steam"><span class="label">Steam</span></a></li>
-					<li><a href="https://github.com/Th3f" class="icon fa-github"><span class="label">Github</span></a></li>
+					<li><a href="https://twitter.com/" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="https://plus.google.com/" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+					<li><a href="https://www.flickr.com/photos/" class="icon fa-flickr"><span class="label">Flickr</span></a></li>
+					<li><a href="https://steamcommunity.com/id/" class="icon fa-steam"><span class="label">Steam</span></a></li>
+					<li><a href="https://github.com/" class="icon fa-github"><span class="label">Github</span></a></li>
 				</ul>
 				<ul class="copyright">
-					<li>&copy; <?php echo date('Y'); ?> - <?php echo site_name(); ?></li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-					<li>Also available within TOR: <a href="http://vkuekxq5wwqoh2fd.onion/">http://vkuekxq5wwqoh2fd.onion/</a></li>
-					<li><a href="/impressum">Impressum</a>, <a href="/datenschutzerklaerung">Privacy Policy</a> und <a href="/disclaimer">Disclaimer</a></li>
+					<li>&copy; <?php echo date('Y'); ?> - <?php echo site_name(); ?></li><li>Design: <a href="http://html5up.net">HTML5 UP</a> - Ported by<a href="https://th3f.de">Th3f.DE</a></li>
 				</ul>
 			</footer>
 
@@ -21,25 +19,6 @@
 			<script src="<?php echo theme_url('assets/js/util.js'); ?>"></script>
 			<!--[if lte IE 8]><script src="<?php echo theme_url('assets/js/ie/respond.min.js'); ?>"></script><![endif]-->
 			<script src="<?php echo theme_url('assets/js/main.js'); ?>"></script>
-			<!--Start Cookie-Disclaimer-->
-			<script type="text/javascript" charset="UTF-8" src="<?php echo theme_url('assets/js/cookies.js'); ?>"></script>
-			<div id="cookie_assistant_container"></div>
-			<!--End Cookie-Disclaimer-->
-			<!-- Piwik -->
-			<script type="text/javascript">
-			  var _paq = _paq || [];
-			  _paq.push(['trackPageView']);
-			  _paq.push(['enableLinkTracking']);
-			  (function() {
-			    var u="//analytics.thiefas.de/";
-			    _paq.push(['setTrackerUrl', u+'piwik.php']);
-			    _paq.push(['setSiteId', 1]);
-			    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-			    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-			  })();
-			</script>
-			<noscript><p><img src="//analytics.thiefas.de/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-			<!-- End Piwik Code -->
 
 	</body>
 </html>
