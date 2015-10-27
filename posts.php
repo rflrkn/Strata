250784b4f29c0f7c4ee75b7596c317f04a7a5437
Rafael Arkenau <Th3f@users.noreply.github.com>
2015-10-27 20:44:45 +0100

    Translated article-preview and removed Uberspace-Badge

--- posts.php
+++ posts.php
@@ -4,24 +4,18 @@
 					<section id="<?php echo article_id(); ?>">
 						<header class="major">
 							<h2><?php echo article_title(); ?></h2>
-							<p>Veröffentlicht am <?php echo article_date(); ?> von <?php echo article_author('real_name'); ?></p>
+							<p>Published on <?php echo article_date(); ?> by <?php echo article_author('real_name'); ?></p>
 						</header>
 						<p><?php echo article_description(); ?></p>
 						<ul class="actions">
-							<li><a href="<?php echo article_url(); ?>" class="button">Weiterlesen...</a></li>
+							<li><a href="<?php echo article_url(); ?>" class="button">More...</a></li>
 						</ul>
 					</section>
 				<?php endwhile; endif; ?>
 		
 				<?php if(has_pagination()): ?>
 				<nav class="pagination">
-					<?php echo posts_prev('<i class="fa fa-chevron-circle-left"></i><span> Ältere Posts</span>', '<a><i class="fa fa-chevron-circle-left"></i><span> Ältere Posts</span></a>'); ?><?php echo posts_next('<span>Neuere Posts </span><i class="fa fa-chevron-circle-right"></i>', '<a><span>Neuere Posts </span><i class="fa fa-chevron-circle-right"></i></a>'); ?>
+					<?php echo posts_prev('<i class="fa fa-chevron-circle-left"></i><span> Older stuff</span>', '<a><i class="fa fa-chevron-circle-left"></i><span> Older Stuff</span></a>'); ?><?php echo posts_next('<span>Newer stuff </span><i class="fa fa-chevron-circle-right"></i>', '<a><span>Newer stuff </span><i class="fa fa-chevron-circle-right"></i></a>'); ?>
 				</nav>
 				<?php endif; ?>
-			<div class="uberspace">
-			<a href="https://uberspace.de" target="_blank">
-				<img src="https://th3f.de/themes/Strata/images/uberspace.png" alt="Hosted on Asteroids!">
-			</a>
-		</div>
-
 <?php theme_include('footer'); ?>
